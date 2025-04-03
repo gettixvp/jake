4c1a6f8e5b5fc6b255cce0b760dd3f17de47ad2f
gettixvp <68188897+gettixvp@users.noreply.github.com>
2025-04-03 15:07:06 +0300

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -1,11 +1,14 @@
-// js/storage.js
 class Storage {
-    static save(key, value) {
-        localStorage.setItem(key, JSON.stringify(value));
+    static load(key, defaultValue) {
+        try {
+            const data = localStorage.getItem(key);
+            return data ? JSON.parse(data) : defaultValue;
+        } catch {
+            return defaultValue;
+        }
     }
 
-    static load(key, defaultValue) {
-        const data = localStorage.getItem(key);
-        return data ? JSON.parse(data) : defaultValue;
+    static save(key, value) {
+        localStorage.setItem(key, JSON.stringify(value));
     }
 }
